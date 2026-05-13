2dd7ffe4b59eb26223536add7a6d7b4e222b5abb
wu-lu120 <1459929060@qq.com>
2026-05-13 12:03:31 +0800

    已经完成docker容器的使用

--- ES-springboot/src/main/java/com/example/esspringboot/config/WebConfig.java
+++ ES-springboot/src/main/java/com/example/esspringboot/config/WebConfig.java
@@ -16,7 +16,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // 配置uploads目录为静态资源
-        String uploadsPath = "D:/java开发路线学习/ES-springboot/uploads/";
+        String uploadsPath = System.getenv("UPLOAD_PATH"); // 从环境变量中获取上传路径--用于docker容器
+        if (uploadsPath == null || uploadsPath.isEmpty()) {       // 本地开发默认值: D:/java开发路线学习/ES/ES-springboot/uploads/
+            uploadsPath = "D:/java开发路线学习/ES/ES-springboot/uploads/";
+        }
+
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:" + uploadsPath)
                 .setCachePeriod(3600)
